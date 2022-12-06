3ce10d0442a5ed3078569f540a64d633254c2acc
Samia Menon <55510385+samia-mnn@users.noreply.github.com>
2022-12-06 01:24:29 -0500

    revamppp

--- sketch2.js
+++ sketch2.js
@@ -1,5 +1,5 @@
 
-let song, nextsong, button, amp, img, stars;
+let song, nextsong, buttton, amp, img;
 let volHistory = [];
 let volHistory2 = [];
 let timeStamps = []; 
@@ -12,18 +12,19 @@ let beethovenSongs =
 
 function preload() {
 
-  song = loadSound('audio/moonlightsonata.mp3'); 
-  nextsong = loadSound('audio/something.mp3');
+  song2 = loadSound('audio/moonlightsonata.mp3'); 
+  song1 = loadSound('audio/something.mp3');
   img = loadImage('bg.png');
-  stars = generateRandom();
+
   getAudioContext().resume();
  // fixed = 'Access-Control-Allow-Origin: https'+song2.substring(4);
-
+  song = song1;
+  nextsong = song2;
 
 //option to pick a new song and save the volhistory
 }
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1500, 600)
   song.play();  
   amp = new p5.Amplitude();
   time = 0;
@@ -36,7 +37,7 @@ function draw() {
   let vol = amp.getLevel();
   time = time+1;
 
-  if (time < 10000)
+  if (time < 600)
   {
   volHistory.push(vol);
   timeStamps.push(song.currentTime()); //find timestamp of song
@@ -44,7 +45,8 @@ function draw() {
   pastx = 0;
   pasty=0;
   for (let x = 0; x < volHistory.length; x++) {
-    stroke(255-x*255/volHistory.length, 255-x*255/volHistory.length, 255)
+    stroke(255, 255, 255-x%255)
+    strokeWeight(5);
     let y = map(volHistory[x], 0, 1, height, 0);
     if (x%600 == 0)
     {
@@ -55,18 +57,8 @@ function draw() {
     pasty = 2*y-800;
    
   }
-  stroke(255,255,255);
-  fill(255,255,255);
-  textSize(40);
-  text("The End of Moonlight", 20, 2*height/3);
-  fill(200, 200,255);
-  noStroke();
-  textSize(20);
-  text("Beethoven wrote his Moonlight Sonata in 1801. At that point, in most of the world, anyone could look up and see a sky full of stars. \nBy the time this sonata ends, it will be 2022.", 20, 2*height/3+50);
-
   }
-  drawStars(stars);
-  if (time >=10000)
+  if (time >=600)
   {
     song.stop();
     frameRate(30);
@@ -116,45 +108,4 @@ function breakSections(volHist){
     sections.append(timestamps[i*600]);
   }
   return sections;
-}
-
-function generateRandom()
-{
-   coordinateTuples = [];
-  for(i = 0 ; i < 100; i++)
-  {
-      coordinateTuples.push([int(random(windowWidth)), int(random(windowHeight))-300]);
-  }
-  return coordinateTuples;
-}
-
-function drawStars(fun)
-{
-  fill(255,255,255);
-  for (i = 0; i < fun.length; i++)
-  {
-
-
-
-    push();
-    translate(fun[i][0], fun[i][1]);
-    rotate(frameCount / -100.0);
-    star(0, 0, 6, 3, 5);
-    pop();
-  }
-}
-
-function star(x, y, radius1, radius2, npoints) {
-  let angle = TWO_PI / npoints;
-  let halfAngle = angle / 2.0;
-  beginShape();
-  for (let a = 0; a < TWO_PI; a += angle) {
-    let sx = x + cos(a) * radius2;
-    let sy = y + sin(a) * radius2;
-    vertex(sx, sy);
-    sx = x + cos(a + halfAngle) * radius1;
-    sy = y + sin(a + halfAngle) * radius1;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
 }
